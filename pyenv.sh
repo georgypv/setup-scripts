e31b5aeecfb82c893e94a95dbfae53586e2b61a9
gapopov <gapopov@sberautotech.ru>
2023-09-18 12:20:50 +0300

    fixed typo, add prerequisites in pyenv setup

--- pyenv.sh
+++ pyenv.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#install prerequisites
+#https://www.adaltas.com/en/2021/06/09/pyrepo-project-initialization/
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
+libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
+
 #https://github.com/pyenv/pyenv#automatic-installer
 sudo curl https://pyenv.run | bash
 
@@ -11,7 +17,7 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
 echo 'eval "$(pyenv init -)"' >> ~/.profile
 
-soruce ~/.bashrc
+source ~/.bashrc
 source ~/.profile
 
 pyenv install 3.8.17
