6534c081fb188df588f4c73d0232256ecce4c8f9
Georgy Popov <originalno93@yandex.ru>
2023-11-18 18:06:48 +0300

    change file from imdb

--- get-test-data.sh
+++ get-test-data.sh
@@ -7,7 +7,7 @@ cd $HOME/test-data;
 echo "Downloading IMDB Dataset...";
 wget https://datasets.imdbws.com/title.basics.tsv.gz -O imdb.title.tsv.gz -nc;
 wget https://datasets.imdbws.com/title.ratings.tsv.gz -O imdb.ratings.tsv.gz -nc;
-wget https://datasets.imdbws.com/title.principals.tsv.gz -O imdb.principals.tsv.gz -nc;
+wget https://datasets.imdbws.com/name.basics.tsv.gz -O imdb.name.tsv.gz -nc;
 
 if [ -f imdb.title.tsv.gz ]; then
     echo "Unpacking .gz files...";
