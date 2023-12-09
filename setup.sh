38564ce45054f5c96f566dbf0659a93c075bd89e
GeorgyPopov <originalno93@yandex.ru>
2023-12-09 14:37:27 +0300

    add rust install

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!bin/bash  
  
-chmod u+x aptinstall.sh programs.sh update-alternatives.sh pyenv.sh poetry.sh get-test-data.sh;
+chmod u+x aptinstall.sh rust.sh programs.sh update-alternatives.sh pyenv.sh poetry.sh get-test-data.sh;
 chmod u+x programs/*.sh;
 
 ./aptinstall.sh
+./rust.sh
 ./programs.sh
 ./update-alternatives.sh
 ./pyenv.sh
